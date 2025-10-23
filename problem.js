334b0caef502e707ae1b908453fb30f10cf87fd7
Tracy <ttram1@my.bcit.ca>
2025-10-23 14:54:44 -0700

    Finished Mid term long answer

--- problem.js
+++ problem.js
@@ -1,15 +1,43 @@
 /*
-Create a function that simulates a simple password checking system. The function will take in a password and a user's input as parameters. It will then compare the user's input to the password.
+Create a function that simulates a simple password checking system. 
+The function will take in a password and a user's input as parameters. 
+It will then compare the user's input to the password.
 
 - If the user's input matches the password exactly, log out "Access Granted!"
 - If the user's input does not match, log out "Access Denied!"
 - If the user's input is "forgot", log out "Here is a hint"
 - If the user's input is "reset", log out "Let's reset your account"
 
-Invoke examples of the function that fullfil each condition listed above. Feel free to work locally using node, and then paste your code into this file when it runs.
+Invoke examples of the function that fullfil each condition listed above. Feel free to work locally using node,
+ and then paste your code into this file when it runs.
 
 CHALLENGE
-- If the password set is "forgot" or "reset", aside from "Access Granted", also log: "This password you set should not be used because it glitches the system".
+- If the password set is "forgot" or "reset", aside from "Access Granted", also log: "This password you set should not be 
+used because it glitches the system".
 - If the password is less than 5 characters, log out "Your password is too short!".
 */
 
+let passW = "password";
+
+function password(password, input) {
+    if (password === input) {
+        console.log("Access Granted!");
+    } else if (password !== input) {
+        console.log("Access Denied!");
+    }
+}
+
+function passwordForgot(_passwordTwo, forgotPassword) {
+    if ((forgotPassword = "forgot")) console.log("Here is a hint");
+}
+
+function passwordReset(_passwordThree, _resetPassword) {
+    if ((forgotPassword = "reset")) {
+        console.log("Let's reset your account");
+    }
+}
+
+password(passW, "password");
+password(passW, "oops");
+passwordForgot(passW, "forgot");
+passwordReset(passW, "reset");
